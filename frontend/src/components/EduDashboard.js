c6529247396e54386b2bdac39664ed5e935a5902
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-10-08 15:44:03 +0530

    Front end done but need to fetch the resource data

--- frontend/src/components/EduDashboard.js
+++ frontend/src/components/EduDashboard.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../components/css/EduDashboard.css'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "../components/css/EduDashboard.css";
+import Navbar from "../components/Navbar";
 
 const EduDashboard = ({ logout }) => {
   return (
     <>
-    <div className='flex'>
-    <div>
-    <li><Link to="/educator/resources">Resource List</Link></li>
-    <li><Link to="/educator/resources">Resource List</Link></li>
-    <li><Link to="/educator/resources">Resource List</Link></li>
-    </div>
-    <div>
-      <h2>Educator Dashboard</h2>
-      <p>Welcome, Educator!</p>
-      <button onClick={logout}>Logout</button>
-    </div>
-    </div>
+      <div className="landing-container">
+        <Navbar />
+        <div className="flex">
+          <div className="sidebar">
+            <div className="content1">
+              <li>
+                <Link to="/educator/resources">Resource List</Link>
+              </li>
+              <li>
+                <Link to="/educator/resources">Resource List</Link>
+              </li>
+              <li>
+                <Link to="/educator/resources">Resource List</Link>
+              </li>
+            </div>
+          </div>
+          <div className="content">
+            <div className="content1">
+              <h2>Educator Dashboard</h2>
+              <p>Welcome, Educator!</p>
+              <button onClick={logout}>Logout</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
